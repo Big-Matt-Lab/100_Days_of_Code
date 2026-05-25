7bc8a0fe9c9a4dcafbfff94c0205687a73148c0d
Matt Labriola <mattlabcode@gmail.com>
2026-05-25 17:00:37 -0400

    task: start refactor of tip calculators

---
+++ tip_calculator.py
@@ -0,0 +1,8 @@
+# By Matt Lab
+"""udemy"""
+import random
+
+friends = ['Alice', 'Bob', 'Charlie', 'David', 'Emanuel']
+
+payer = random.choice(friends)
+print(f"{payer} should pay the bill today!")
