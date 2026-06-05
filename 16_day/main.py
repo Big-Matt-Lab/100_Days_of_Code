3d8e8b2e9302304f58c83cc0c67b06e048b14e1e
Matt Lab <matt-lab-ed@gmail.com>
2026-06-04 23:23:29 -0400

    refactor: rename coffee_oop to day_16_coffee_maker, add prettytable example, clean up config files

---
+++ 16_day/main.py
@@ -0,0 +1,11 @@
+from prettytable import PrettyTable
+
+my_dict = {"Pokemon": ('Pikachu', 'Squirtle', 'Charmander'), "Type": ('Electric', 'Water', 'Fire')}
+
+table = PrettyTable()
+for col, rows in my_dict.items():
+    table.add_column(col, rows)
+table.align = "l"
+
+print(table)
+cd
