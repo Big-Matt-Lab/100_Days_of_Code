b677d4c7426a2a9a08772df401ada9a5585b7727
Matt Lab <matt-lab-ed@gmail.com>
2026-08-18 15:01:17 -0400

    feat: Add Day 32 Email Birthday Wisher project Introduces the "Day 32: Email Birthday Wisher" project, including both `hard` and `solution` directories. This addition comprises: - : Data file for storing birthday information. `birthdays.csv` - : Directory containing various letter templates (, , ) for custom birthday messages. `letter_templates/``letter_1.txt``letter_2.txt``letter_3.txt` - : The core script responsible for reading birthday data, selecting a random letter, and preparing birthday emails. `main.py` - : Specifies project-specific Python dependencies. `requirements.txt`
    Also updates and to incorporate new dependencies required by this project. `pyproject.toml``uv.lock`

---
+++ Day_32_emails/hard/main.py
@@ -0,0 +1,54 @@
+##################### Hard Starting Project ######################
+
+# 1. Update the birthdays.csv with your friends & family's details. 
+# HINT: Make sure one of the entries matches today's date for testing purposes. 
+
+# 2. Check if today matches a birthday in the birthdays.csv
+# HINT 1: Only the month and day matter. 
+# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
+# birthdays_dict = {
+#     (month, day): data_row
+# }
+#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
+# if (today_month, today_day) in birthdays_dict:
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# HINT: https://www.w3schools.com/python/ref_string_replace.asp
+
+# 4. Send the letter generated in step 3 to that person's email address.
+# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+
+# Get csv
+import os
+import random
+import smtplib
+import zoneinfo
+from datetime import datetime
+
+import pandas
+
+MY_EMAIL = "mattlabcode@gmail.com"
+MY_PASSWORD = os.getenv("MY_GOOGLE_APP_PASSWORD")
+SMTP_SERVER = "smtp.gmail.com"  # The hostname of the SMTP server.
+SMTP_PORT = 587
+
+today = datetime.now(tz=zoneinfo.ZoneInfo("America/New_York"))
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
